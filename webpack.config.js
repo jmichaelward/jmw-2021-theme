55d31e236c590f7f716b5f1220b97d5ae6b1458a
Jeremy Ward <jeremy@jmichaelward.com>
2020-03-21 20:41:07 -0500

    Initially configure CSS loaders.

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,51 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: "./assets/src/js/index.js",
-  output: {
-    path: path.resolve(__dirname, 'assets/dist/js'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: [
+      "./assets/src/js/index.js",
+      "./assets/src/scss/main.scss",
+    ],
+    output: {
+      path: path.resolve(__dirname, 'assets/dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
           }
-        }
-      }
+        },
+        // @see https://github.com/webpack-contrib/sass-loader
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+              }
+            },
+            {
+              loader: 'sass-loader',
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFileName: '[id].css',
+      })
     ]
   }
 };
