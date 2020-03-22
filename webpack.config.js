3a129540dfd14dfb6d86d8b8af83770cabaf1879
Jeremy Ward <jeremy@jmichaelward.com>
2020-03-21 19:38:17 -0500

    Configure babel-loader for webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,18 @@ module.exports = {
     path: path.resolve(__dirname, 'assets/dist/js'),
     filename: 'bundle.js'
   },
-  mode: 'production'
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 };
