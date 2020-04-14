25f56d4c523dcb39f32206a5500cfa28b8eabd21
Jeremy Ward <jeremy@jmichaelward.com>
2020-04-14 12:42:16 -0500

    Remove GitHub link from footer and add range to copyright.

--- footer.php
+++ footer.php
@@ -9,10 +9,8 @@
 </div><!-- .site-wrapper -->
 <footer class="site-footer">
 	<div class="site-footer__inner">
-		<p class="site-footer__copyright">&copy; <?php echo esc_html( gmdate( 'Y' ) ); ?>
-			<a href="https://github.com/jmichaelward" rel="me">
-				<?php esc_html_e( 'J. Michael Ward', 'jmw-2020-theme' ); ?>
-			</a>
+		<p class="site-footer__copyright">&copy;2009-<?php echo esc_html( gmdate( 'Y' ) ); ?>
+			<?php esc_html_e( 'J. Michael Ward', 'jmw-2020-theme' ); ?>
 		</p>
 	</div>
 </footer>
