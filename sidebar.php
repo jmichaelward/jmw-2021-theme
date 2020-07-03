83459e8df3cb0a6d430e2ae2e48d8e76742c17e6
Jeremy Ward <jeremy@jmichaelward.com>
2020-07-03 18:24:11 -0500

    Wrap sidebar sections in divs.

--- sidebar.php
+++ sidebar.php
@@ -1,10 +1,15 @@
 <aside class="sidebar sidebar--main">
-	<?php echo get_avatar( 1, 640, false, 'J. Michael Ward' ); ?>
-	<p>
-		<a href="<?php echo esc_url( home_url() ); ?>"
-		   title="<?php bloginfo( 'name' ); ?>">
-			<span><?php bloginfo( 'name' ); ?></span>
-		</a>
-	</p>
-	<p><?php bloginfo( 'description' ); ?></p>
+	<div class="sidebar__image">
+		<?php echo get_avatar( 1, 640, false, 'J. Michael Ward' ); ?>
+	</div>
+
+	<div class="sidebar__details">
+		<p>
+			<a href="<?php echo esc_url( home_url() ); ?>"
+			   title="<?php bloginfo( 'name' ); ?>">
+				<span><?php bloginfo( 'name' ); ?></span>
+			</a>
+		</p>
+		<p><?php bloginfo( 'description' ); ?></p>
+	</div>
 </aside>
