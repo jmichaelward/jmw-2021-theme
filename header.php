6663c016deaf3e48d746c91362eebba406b766dd
Jeremy Ward <jeremy@jmichaelward.com>
2021-01-30 16:43:33 -0600

    Fix post launch theme issues.

--- header.php
+++ header.php
@@ -9,11 +9,13 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width">
+	<link rel="shortcut icon" href="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/favicon.ico" />
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+	<?php do_action( 'wp_body_open' ); ?>
 	<div class="site-container">
 		<header class="site-header">
 			<div class="site-header__inner">
--- src/Theme.php
+++ src/Theme.php
@@ -56,6 +56,7 @@ class Theme extends ServiceRegistrar implements Hookable {
 	 */
 	public function register_hooks() {
 		add_action( 'after_setup_theme', function() {
+			add_theme_support( 'title-tag' );
 			add_theme_support( 'post-thumbnails' );
 		} );
 	}
